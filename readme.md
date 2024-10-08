04cbb1303e87668fee244601ab20fac460233b3e
extrange <29305375+extrange@users.noreply.github.com>
2024-10-08 19:14:45 +0800

    run as user instead

--- readme.md
+++ readme.md
@@ -3,5 +3,5 @@
 Sets up a reverse shell on remote machines, using my server as the gateway.
 
 ```sh
-curl https://raw.githubusercontent.com/extrange/reverse-shell/main/setup.sh | sudo bash
+curl https://raw.githubusercontent.com/extrange/reverse-shell/main/setup.sh | bash
 ```
--- setup.sh
+++ setup.sh
@@ -1,27 +1,20 @@
 #! /usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-USER=sagemaker-user
 SERVER_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEZXrm0AXgoOcJWckgr/ZgYVdHKrJHJg5G52bIx6zc4b user@server"
-USER_DIR="/home/$USER"
 
-apt update
+sudo apt update
 
-apt install openssh-server -y
+sudo apt install openssh-server -y
 
-ssh-keygen -A -t ed25519
+sudo ssh-keygen -A
 
-service ssh --full-restart
+sudo service ssh --full-restart
 
-touch "$USER_DIR/.ssh/authorized_keys"
-touch "$USER_DIR/.ssh/known_hosts"
-echo "$SERVER_KEY" >> "$USER_DIR/.ssh/authorized_keys"
-echo "$SERVER_KEY" >> "$USER_DIR/.ssh/known_hosts"
-chown -R "$USER" "$USER_DIR/.ssh"
-chmod 0600 -R "USER_DIR/.ssh"
+touch "$HOME/.ssh/authorized_keys"
+touch "$HOME/.ssh/known_hosts"
+echo "$SERVER_KEY" >> "$HOME/.ssh/authorized_keys"
+echo "$SERVER_KEY" >> "$HOME/.ssh/known_hosts"
+chown -R "$USER" "$HOME/.ssh"
+chmod 0600 -R "HOME/.ssh"
 
 ssh -R localhost:9001:localhost:22 -p 39483 user@ssh.nicholaslyz.com -N
