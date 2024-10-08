4712fab7e05fe77698d470133ed466444ce231f6
extrange <29305375+extrange@users.noreply.github.com>
2024-10-08 19:11:38 +0800

    fix permissions

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+USER=sagemaker-user
 SERVER_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEZXrm0AXgoOcJWckgr/ZgYVdHKrJHJg5G52bIx6zc4b user@server"
+USER_DIR="/home/$USER"
 
 apt update
 
@@ -15,9 +17,11 @@ ssh-keygen -A -t ed25519
 
 service ssh --full-restart
 
-touch ~/.ssh/authorized_keys
-touch ~/.ssh/known_hosts
-echo "$SERVER_KEY" >> ~/.ssh/authorized_keys
-echo "$SERVER_KEY" >> ~/.ssh/known_hosts
+touch "$USER_DIR/.ssh/authorized_keys"
+touch "$USER_DIR/.ssh/known_hosts"
+echo "$SERVER_KEY" >> "$USER_DIR/.ssh/authorized_keys"
+echo "$SERVER_KEY" >> "$USER_DIR/.ssh/known_hosts"
+chown -R "$USER" "$USER_DIR/.ssh"
+chmod 0600 -R "USER_DIR/.ssh"
 
 ssh -R localhost:9001:localhost:22 -p 39483 user@ssh.nicholaslyz.com -N
